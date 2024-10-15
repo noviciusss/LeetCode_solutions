de31d4f020085d46301d969518244d031ae7c398
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-10-15 11:50:36 +0530

    Time: 35 ms (6.68%), Space: 24.6 MB (5.47%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>hash;
+        for(int i=0;i<nums.size();i++){
+            hash[nums[i]]++;
+        }
+        for(auto it:hash){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
