8f0f089e9ab825376bb2f5a3b899e6f8a495411c
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-11-28 19:19:59 +0530

    Time: 9 ms (48%), Space: 31.6 MB (50.14%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,7 +5,8 @@ public:
         int n =nums.size();
        int multi1=nums[n-1]*nums[n-2]*nums[n-3]; //negative condition me first 2 after sorted then last one give best negative;
        int multi2=nums[0]*nums[1]*nums[n-1];
-       int ans= max(multi1,multi2);
-       return ans;
+    //    int ans= max(multi1,multi2);
+    //    return ans;
+    return(max(multi1,multi2));
     }
 };
