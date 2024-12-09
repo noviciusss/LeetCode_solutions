738858504db9cf31c6971d9216f6593c431291d0
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-12-09 20:19:20 +0530

    Time: 1 ms (19.08%), Space: 17.3 MB (88.11%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -3,7 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int ans=0;
         for(auto detail:details){
-         int age=(detail[11]-'0')*10+(detail[12]-'0');
+         int age=(detail[11]-'0')*10+(detail[12]-'0'); //so if we use -'0' it convert s to numeric vakue
         if(age>60){ans+=1;}}
         return ans;
     }
