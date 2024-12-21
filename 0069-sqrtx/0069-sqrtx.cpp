c52718c9f427d8657d12fca8dfd0e975b1d1fa23
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-12-21 23:16:19 +0530

    Time: 3 ms (19.27%), Space: 8.6 MB (6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int low=1,high=x;
+        while(low<=high){
+            double mid=low + (high - low) / 2;;
+            if(mid*mid==x){return mid;}
+            if(mid*mid<=x){
+                low=mid+1;
+            }else{high=mid-1;}
+        }
+        return high;
     }
 };
