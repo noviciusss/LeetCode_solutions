a5ed7995f2a8a69cfd026b6388b502e174e31428
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2025-01-14 17:51:43 +0530

    Time: 4 ms (69.55%), Space: 85.7 MB (85.75%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        vector<int>ans;
+        // vector<int>ans;
+        // for(int i=0;i<A.size();i++){
+        //     int sum=0;
+        //     for(int j=0;j<=i;j++){
+        //         for(int k=0;k<=i;k++){
+        //             if(A[j]==B[k]){
+        //                 sum++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     ans.push_back(sum);
+        // }
+        // return ans;
+        int n=A.size();
+        vector<int>chk(n+1,0),ans(n);
+        int cnt=0;
         for(int i=0;i<A.size();i++){
-            int sum=0;
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<=i;k++){
-                    if(A[j]==B[k]){
-                        sum++;
-                        break;
-                    }
-                }
+            chk[A[i]]++;
+            if(chk[A[i]]==2){
+                cnt++;
             }
-            ans.push_back(sum);
+            chk[B[i]]++;
+            if(chk[B[i]]==2){
+                cnt++;
+            }
+            ans[i]=cnt;
         }
         return ans;
     }
