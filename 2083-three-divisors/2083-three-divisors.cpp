266009693a1181157f3bdd339b00b7ad053404d8
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2025-05-13 21:11:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.62%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int ans =1;
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){ans++;}
+        }
+        if(ans==3)return true;
+        return false;
+    }
+};
