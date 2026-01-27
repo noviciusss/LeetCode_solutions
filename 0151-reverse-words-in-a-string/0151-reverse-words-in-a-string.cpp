812589a543ad879e84df6879658c39c8ac457792
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-01-27 23:05:10 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int left =0;
-        int right=s.length()-1;
-        string temp="";
-        string ans="";
-        while(left<=right){
-            char ch =s[left];
-            if(ch!=' '){
-                temp+=ch;
-            }
-            else if(ch==' '){
-                if(temp!=""){
-                    if(ans!="")ans=temp+" "+ans;
-                    else ans=temp;
-                    temp="";
-            }}
-            left++;
-        }
-        if(temp!=""){
-            if(ans!="")ans=temp+' '+ans;
-            else ans=temp;
-
+        string ans;
+        int i = s.size()-1;
+        while(i>=0){
+            while(i>=0 && s[i]==' ')i--;
+            if(i<0)break;
+            int j=i;
+            while(i>=0 && s[i]!=' ')i--;
+            if(!ans.empty()){ans.push_back(' ');}
+            ans.append(s.substr(i+1,j-i)); // so ye ik inbuild function hai subtr(start,len) kidhar se suru kre aur kitna aage tak thats why ik piche jaye means " "pe pauch kar taki ki while process na intrepret ho i+1se start uska end --> word count is end-start+1 --> j-(i+1)+1 = j-i
         }
         return ans;
     }
