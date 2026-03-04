5d9bececbb75a455fa51d3f9e1bc7851c002cabd
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-03-05 01:19:56 +0530

    Time: 0 ms (100%), Space: 9.9 MB (78.72%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-       
-        
-        while(stones.size()>1){
-            int n = stones.size();
-            sort(stones.begin(),stones.end());
-            if(stones[n-1] ==stones[n-2]){
-                stones.pop_back();
-                stones.pop_back();
-            }
-            else{
-                int pu=stones[n-1]-stones[n-2];
-                stones.pop_back();
-                stones.pop_back();
-                stones.push_back(pu);}
+    int lastStoneWeight(vector<int>& stones) { 
+        // while(stones.size()>1){
+        //     int n = stones.size();
+        //     sort(stones.begin(),stones.end());
+        //     if(stones[n-1] ==stones[n-2]){
+        //         stones.pop_back();
+        //         stones.pop_back();
+        //     }
+        //     else{
+        //         int pu=stones[n-1]-stones[n-2];
+        //         stones.pop_back();
+        //         stones.pop_back();
+        //         stones.push_back(pu);}
+        // }
+        // if(stones.size()==1){return stones[0];}
+        // return 0;
+        priority_queue<int>pq(stones.begin(),stones.end());
+        while(pq.size()>1){
+            int m1=pq.top();
+            pq.pop();
+            int m2=pq.top();
+            pq.pop();
+            int diff = m1-m2;
+            if(diff>0){pq.push(diff);}
         }
-        if(stones.size()==1){return stones[0];}
-        return 0;
+       return pq.empty() ? 0 : pq.top();
     }
 };
