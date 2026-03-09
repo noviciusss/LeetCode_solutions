96be341f67ca21e4a6c4bf54612adac440ebe76e
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-03-09 20:43:41 +0530

    Time: 0 ms (100%), Space: 13.2 MB (98.57%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -5,11 +5,23 @@ public:
         // int n = nums.size();
         // int ans = (nums[n-1]-1) * (nums[n-2]-1);
         // return ans;
-        priority_queue<int>pt(nums.begin(),nums.end());
-        int num1 = pt.top()-1;
-        pt.pop();
-        int num2 = pt.top()-1;
-        return num1*num2;
+       
+        // priority_queue<int>pt(nums.begin(),nums.end());
+        // int num1 = pt.top()-1;
+        // pt.pop();
+        // int num2 = pt.top()-1;
+        // return num1*num2;
+        int max1 =0,max2 = 0;
+        for(int num :nums){
+            if(num>=max1){
+                max2=max1;
+                max1=num;
+            }
+            else if(num>=max2){
+                max2=num;
+            }
+        }
+        return (max1-1)*(max2-1);
 
     }
 };
