c683264e8f6bcce30dd6708f1e676659572f78cd
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-03-23 17:22:20 +0530

    Time: 0 ms (100%), Space: 8.1 MB (15.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n<=1)return n;
-        return fib(n-1)+fib(n-2);
+        vector<int>memo(n+1,-1);
+        return dp(n,memo);
+
+    }
+    int dp(int n,vector<int>&memo){
+        if(n<=1)return n;
+        if(memo[n]!=-1)return memo[n];
+        return memo[n] = dp(n-1,memo)+dp(n-2,memo);
     }
 };
