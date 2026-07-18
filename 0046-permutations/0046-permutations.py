ba9197edde9631172497b9fbbb6040167cf9fddf
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-07-18 22:04:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (65.63%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path  = []
+        used = [False]*len(nums)
+        def bt():
+            if len(path)==len(nums):
+                res.append(path[:])
+                return 
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                path.append(nums[i])
+                used[i]=True
+                bt()
+                path.pop()
+                used[i]=False
+        bt()
+        return res
