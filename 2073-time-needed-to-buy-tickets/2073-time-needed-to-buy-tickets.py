6d832897646d6e1609d1f20d9decbb1afa649532
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-07-27 10:53:39 +0530

    Time: 0 ms (100%), Space: 19.4 MB (22.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -3,7 +3,7 @@ class Solution:
         ans = 0
         for i,n in enumerate(tickets):
             if i<=k:
-                ans+=min(n,tickets[k])
+                ans+=min(n,tickets[k]) # if early then will be taken all if less then k else till k
             else:
-                ans+=min(n,tickets[k]-1)
+                ans+=min(n,tickets[k]-1) # if after k then will run for 1 less then k as k end early means less than k then then whole or k-1
         return ans
