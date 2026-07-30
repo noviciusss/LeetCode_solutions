fd9cb455fd0ef4bf58210240705265e3ea4f9558
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2026-07-30 19:10:55 +0530

    Time: 43 ms (68.47%), Space: 19.2 MB (39.18%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,10 +1,11 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        n = len(address)
-        arr = []
-        for i in address:
-            if i is '.':
-                arr.append('[.]')
-            else:
-                arr.append(i)
-        return "".join(arr)
+        # n = len(address)
+        # arr = []
+        # for i in address:
+        #     if i is '.':
+        #         arr.append('[.]')
+        #     else:
+        #         arr.append(i)
+        # return "".join(arr)
+        return address.replace('.','[.]')
